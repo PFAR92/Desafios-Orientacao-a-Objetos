af638f97254c885b8463c20c3b517902e13a0a69
PFAR92 <paulofelipetj@hotmail.com>
2022-09-30 14:12:06 -0300

    30/09/22 14:11

--- herancaAssociacaoInterface/src/App.java
+++ herancaAssociacaoInterface/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Vendedor vendedor = (Vendedor) new Funcionario();
+        Funcionario vdas = (Funcionario) new Vendedor();
     }
 }
