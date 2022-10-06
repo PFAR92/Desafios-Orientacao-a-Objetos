e7ec369a41ff08c70f3c19ccb7a80818c0496680
PFAR92 <paulofelipetj@hotmail.com>
2022-10-06 15:37:51 -0300

    06/10/22

--- exercicioPolimorfismo/src/Peixe.java
+++ exercicioPolimorfismo/src/Peixe.java
@@ -1,3 +1,35 @@
 public class Peixe extends Animal{
+    private String corScama;
+
+    public String getCorScama() {
+        return corScama;
+    }
+
+    public void setCorScama(String corScama) {
+        this.corScama = corScama;
+    }
+    
+    public void soltarBolha(){
+        System.out.println("Soltou uma bolha");
+    }
+
+    @Override
+    public void locomover() {
+        System.out.println("Nadando");
+        
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Comendo Substãncias");
+        
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Peixe não faz som");
+        
+    }
+
     
 }
