55158376f0131c02e272ee2f2fe4ba9cb94761e5
PFAR92 <paulofelipetj@hotmail.com>
2022-10-06 22:48:11 -0300

    06/10/22

--- exercicioPolimorfismo/src/App.java
+++ exercicioPolimorfismo/src/App.java
@@ -1,27 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Mamifero m = new Mamifero();
-        Reptil r = new Reptil();
-        Peixe p = new Peixe();
-        Ave a = new Ave();
 
-        m.setPeso(35.3f);
-        m.setCorPelo("Marrom");
-        m.alimentar();
-        m.locomover();
-        m.emitirSom();
-
-        a.locomover();
-        p.locomover();
-        r.locomover();
-
-        Cancuru cg = new Cancuru();
         Cachorro ca = new Cachorro();
-        Cobra co = new Cobra();
-        Tartaruga ta = new Tartaruga();
-        Goldfish go = new Goldfish();
-        Arara ar = new Arara();
-        cg.locomover();
-        ca.locomover();
+        Lobo lo = new Lobo();
+
+        ca.emitirSom();
+        lo.emitirSom();
+
+        ca.reagir("Olá");
+        ca.reagir("Vai apanhar");
+        ca.reagir(11, 45);
+        ca.reagir(21, 0);
+        ca.reagir(true);
+        ca.reagir(false);
+        ca.reagir(2, 12.5f);
+        ca.reagir(17, 4.5f);
+
+
+
+
     }
 }
