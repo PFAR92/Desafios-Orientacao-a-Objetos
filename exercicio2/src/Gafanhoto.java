1f401567a8d6abaa6ffd3c3d2ef86232af603098
PFAR92 <paulofelipetj@hotmail.com>
2022-10-07 10:06:52 -0300

    ex2

---
+++ exercicio2/src/Gafanhoto.java
@@ -0,0 +1,31 @@
+public class Gafanhoto extends Pessoa{
+    private String login;
+    private int totAssistido;
+
+    public void viuMaisUm(){
+
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getTotAssistido() {
+        return totAssistido;
+    }
+
+    public void setTotAssistido(int totAssistido) {
+        this.totAssistido = totAssistido;
+    }
+
+    @Override
+    protected void ganharExperiencia() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
---
+++ exercicio2/src/Pessoa.java
@@ -0,0 +1,8 @@
+public abstract class Pessoa {
+    protected String nome;
+    protected int idade;
+    protected String sexo;
+    protected int esperiencia;
+
+    protected abstract void ganharExperiencia();
+}
